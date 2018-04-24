5b077105298f94434bf3b660ebc13af93ede6777
Alejandro López <oalopezp@unal.edu.co>
2018-04-24 09:46:08 -0500

    Css Arreglos

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,3 +1,6 @@
 import Controller from '@ember/controller';
+import Ember from 'ember';
 
-export default Controller.extend({});
+export default Controller.extend({
+  isBienvenida: Ember.computed.equal('currentRouteName', 'bienvenida')
+});
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,17 +1,21 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import {inject as service} from '@ember/service';
 
 export default Route.extend({
   session: service(),
   usuarioActual: service('usuario-actual'),
-  beforeModel: function() {
-    return this.get('session').fetch().catch(function() {})
-  },
-  actions:{
-    salir: function() {
+  beforeModel:
+    function () {
+      return this.get('session').fetch().catch(function () {
+      })
+    }
+  ,
+  actions: {
+    salir: function () {
       this.get('session').close();
-      this.replaceWith('bienvenida')
+      this.replaceWith('bienvenida');
       this.get('usuarioActual').usuario = []
     }
   }
-});
+})
+;
